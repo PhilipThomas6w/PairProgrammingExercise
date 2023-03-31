e2530f275f94a5c23ec2a6901839d183c77c4c58
FuzzyLogician27 <p.ardaghwalter@hotmail.com>
2023-03-31 15:19:49 +0100

    Patrick Commit 1

--- MoneyProblemApp/MoneyProblemApp/Program.cs
+++ MoneyProblemApp/MoneyProblemApp/Program.cs
@@ -10,8 +10,15 @@ public class Program
     public static int[] CurrencyCalculator(decimal moneyIn)
     {
         int [] denominations = {200, 100, 50, 20, 10, 5, 2, 1};
-        
+        int[] counts = new int[8];
+        int remainder = (int)(moneyIn * 100);
 
+        for(int i = 0; i < 8; i++)
+        {
+            counts[i] = remainder / denominations[i];
+        }
+
+        return counts;
     }
 
 
